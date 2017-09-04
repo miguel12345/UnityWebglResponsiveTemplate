b7b593f0b3311c7612353d5df5a6db01349eaeb0
Miguel Ferreira <amigueloferreira@gmail.com>
2017-09-04 09:28:56 +0100

    Updated template loader .js file and package

--- Assets/WebGLTemplates/Responsive/responsiveMinimalTemplateLoader.js
+++ Assets/WebGLTemplates/Responsive/responsiveMinimalTemplateLoader.js
@@ -3,6 +3,7 @@
  */
 
 var gameInstance = null;
+var gameContainer = null;
 var gameCanvas = null;
 var loadingContainer = null;
 var loadingText = null;
@@ -25,34 +26,49 @@ function updateLoadingText(timestamp) {
     window.requestAnimationFrame(updateLoadingText);
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    loadingContainer = document.body.querySelector("#loadingContainer");
-    loadingText = loadingContainer.querySelector("span");
-    window.requestAnimationFrame(updateLoadingText);
-});
 
 function handleResize() {
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
-    gameCanvas.width = windowWidth;
-    gameCanvas.height = windowHeight;
+    console.log("window.innerHeight "+window.innerHeight);
+
+    if(gameCanvas != null) {
+      gameCanvas.width = windowWidth;
+      gameCanvas.height = windowHeight;
+    }
+
+    gameContainer.style.height = windowHeight+"px";
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    loadingContainer = document.body.querySelector("#loadingContainer");
+    gameContainer = document.body.querySelector("#gameContainer");
+    loadingText = loadingContainer.querySelector("span");
+    window.requestAnimationFrame(updateLoadingText);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+});
+
 
 function OnRuntimeIntialized() {
     runtimeInitialized = true;
     gameCanvas = gameInstance.container.querySelector("canvas");
-    window.addEventListener("resize", handleResize);
     handleResize();
+}
+
+function dummyProgressFunction() {
+
 }
 
 function instantiateUnity(url) {
 
     var windowWidth = window.innerWidth-2; // -2 accounts for the border
     var windowHeight = window.innerHeight-2;
-
     gameInstance = UnityLoader.instantiate("gameContainer", url,  {
         width:windowWidth,
         height: windowHeight,
+        margin: 0,
+        onProgress: dummyProgressFunction,
         Module: {
             onRuntimeInitialized: OnRuntimeIntialized
         }});
